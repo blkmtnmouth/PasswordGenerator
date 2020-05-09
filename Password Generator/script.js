63f0925a7e1a981c04a3a35ed6665592a6342b3a
blkmtnmouth <59021081+blkmtnmouth@users.noreply.github.com>
2020-05-08 21:32:20 -0400

    Finished
    Finished with console.log added.

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -10,55 +10,65 @@ var generateBtn = document.querySelector("#generate");
     var upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
     var numbChar = "1234567890"; 
     var specChar = "!@#$%^&*()_+"; 
+    var masterChar = "";
+    var passWord = "";
 
     var passLength = prompt ("How long do you want your password to be? "); 
-
-    if (passLength < 8 || passLength > 128){
-      alert ("your password needs to be 8-128 characters. ");
-    }
-
     var lowerCase = confirm ("Do you want lowercase letters? ");
     var upperCase = confirm ("Do you want uppercase letters? ");
     var numReg = confirm ("Do you want numbers? ");
     var special  = confirm ("Do you want special characters in your password? ");
 
-
-
+    if (passLength < 8 || passLength > 128){
+      alert ("your password needs to be 8-128 characters. ");
+    }
 
     else if (lowerCase === false && upperCase === false && numReg === false && special === false){
       alert ("You need to pick at least one option ");
     }
 
-    console.log(lowerCase);
-    console.log(upperCase);
-    console.log(numReg);
-    console.log(special);
-    console.log(passLength);
-
-
     else {
 
-      
+      if (lowerCase === true){
+        masterChar = masterChar + lowChar;
+        console.log(masterChar);
+      }   
+
+      if (upperCase === true){
+        masterChar = masterChar + upChar;
+        console.log(masterChar); 
+      }
+
+      if (numReg === true){
+        masterChar = masterChar + numbChar;
+        console.log(masterChar);
+      }
+
+      if (special === true){
+        masterChar = masterChar + specChar;
+        console.log(masterChar);
+      }
+
+      var i; 
+      for (i = 0; i < passLength; i++){
+       var index = Math.floor(Math.random() * masterChar.length);
+        passWord = passWord + masterChar.charAt(index);
+        console.log(i);
+        console.log(index);
+        console.log(masterChar.charAt(index));
+        console.log(passWord);
+      } 
+
+      return passWord;
 
     }
 
-      
-
-    
-      
-    
-    
-
   }
 
-  
-  
-  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
