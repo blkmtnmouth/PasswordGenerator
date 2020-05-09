25c3794519eb1ff39b3177ee76e72a5527d017d6
blkmtnmouth <59021081+blkmtnmouth@users.noreply.github.com>
2020-05-08 20:39:45 -0400

    Update
    Added more variables.

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -4,34 +4,56 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Write password to the #password input
-  function getUserInput() {
+  function generatePassword() {
+
+    var lowChar = "abcdefghijklmnopqrstuvwxyz"; 
+    var upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+    var numbChar = "1234567890"; 
+    var specChar = "!@#$%^&*()_+"; 
 
     var passLength = prompt ("How long do you want your password to be? "); 
+
+    if (passLength < 8 || passLength > 128){
+      alert ("your password needs to be 8-128 characters. ");
+    }
 
     var lowerCase = confirm ("Do you want lowercase letters? ");
     var upperCase = confirm ("Do you want uppercase letters? ");
     var numReg = confirm ("Do you want numbers? ");
     var special  = confirm ("Do you want special characters in your password? ");
 
-    if (passLength < 8 || passLength > 128){
-      alert ("your password needs to be 8-128 characters. ");
-    }
+
+
 
     else if (lowerCase === false && upperCase === false && numReg === false && special === false){
-        alert ("You need to pick at least one option ");
+      alert ("You need to pick at least one option ");
     }
-    
+
+    console.log(lowerCase);
+    console.log(upperCase);
+    console.log(numReg);
+    console.log(special);
+    console.log(passLength);
+
+
     else {
-      generatePassword();
+
+      
+
     }
 
-  }
-
-  function generatePassword(){
+      
 
     
+      
+    
+    
+
   }
 
+  
+  
+  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -39,4 +61,4 @@ var generateBtn = document.querySelector("#generate");
 
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", getUserInput);
+generateBtn.addEventListener("click", generatePassword);
